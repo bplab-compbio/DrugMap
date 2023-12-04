3fe1f36ae79434565cfb5c5c251b2d83fae64f3f
bplab-compbio <146362200+bplab-compbio@users.noreply.github.com>
2023-12-04 16:52:20 -0500

    Update perm_integrate.m

--- CSEA/perm_integrate.m
+++ CSEA/perm_integrate.m
@@ -1,6 +1,6 @@
-function [p] = perm_integrate(null_dist,observed,sidedness)
+function [p] = perm_integrate2(null_dist,observed)
 % Integrates a null distribution of permutations to calculate p value
-% Need to provide observed as the value in the right tail
+% Need to provide observed as the value in tail
     
     swtch = 0;
 
@@ -13,31 +13,30 @@ function [p] = perm_integrate(null_dist,observed,sidedness)
     % abscissae of interpolated gaussian
     for j = 1:length(b) - 1, bb = [bb;(b(j + 1) + b(j))/2]; end    
 
-    % if we don't have enough abscissae, add a few
-    if length(bb) == 2, swtch = 1; bb = [bb;2]; end
-    if length(bb)  == 1, swtch = 2; bb = [bb;[1;2]]; end
-
-    % get ordinates of null distribution
-    v = h.Values;
-    v = v/sum(v);
-
-    % if no intersections in null distribution, add a bit of density
-    if swtch == 1, v = [v,0.001]; end
-    if swtch == 2, v = [v,0.001,0.001]; end
-
-    % fit gaussian
-    f = fit(bb,v','gauss1');
-    fun = @(x,a,b,c) a*exp(-1*((x-b)/c).^2);
 
 
-    % integrate either right tail or both tails
-    if sidedness == 1
-        tot = integral(@(x) fun(x,f.a1,f.b1,f.c1),-1*inf,inf);
-        nom = integral(@(x) fun(x,f.a1,f.b1,f.c1),observed,inf);
+    % if we have enough abscissae
+    if length(bb) > 3
+
+        % get ordinates of null distribution
+        v = h.Values;
+        v = v/sum(v);
+    
+        % if no intersections in null distribution, add a bit of density
+        if swtch == 1, v = [v,0.001]; end
+        if swtch == 2, v = [v,0.001,0.001]; end
+    
+        % fit gaussian
+        f = fit(bb,v','gauss1');
+        fun = @(x,a,b,c) a*exp(-1*((x-b)/c).^2);
+    
+    
+        % integrate
+        tot = integral(@(x) fun(x,f.a1,f.b1,f.c1),-1*inf,inf,'RelTol',1e-4,'AbsTol',1e-6);
+        nom = integral(@(x) fun(x,f.a1,f.b1,f.c1),observed,inf,'RelTol',1e-4,'AbsTol',1e-6);   
+        
         p = nom/tot;
-    elseif sidedness == 2
-        tot = integral(@(x) fun(x,f.a1,f.b1,f.c1),-1*inf,inf);
-        nom = 2 * integral(@(x) fun(x,f.a1,f.b1,f.c1),observed,inf);
-        p = nom/tot;        
+    else
+        p = NaN;
     end
 end
