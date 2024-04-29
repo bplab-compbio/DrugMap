c107b9f4b9d6fd02925e65a17fa1ffdc55471de0
bplab-compbio <146362200+bplab-compbio@users.noreply.github.com>
2024-04-29 15:29:17 -0400

    Update perm_integrate.m
    Fixed bug during integration step in p-value calculation

--- CSEA/perm_integrate.m
+++ CSEA/perm_integrate.m
@@ -22,11 +22,8 @@ function [p] = perm_integrate(null_dist,observed)
         f = fit(bb,v','gauss1');
         fun = @(x,a,b,c) a*exp(-1*((x-b)/c).^2);
         
-        % integrate
-        tot = integral(@(x) fun(x,f.a1,f.b1,f.c1),-1*inf,inf,'RelTol',1e-4,'AbsTol',1e-6);
-        nom = integral(@(x) fun(x,f.a1,f.b1,f.c1),observed,inf,'RelTol',1e-4,'AbsTol',1e-6);   
-        
-        p = nom/tot;
+        % integrate        
+        p = integral(@(x) fun(x,f.a1,f.b1,f.c1),observed,inf,'RelTol',1e-4,'AbsTol',1e-6);   
 
     % else, skip p-value calculation
     else
